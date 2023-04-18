9ea43b5361abfa49e05ee8657fc395f69fb1489d
Alvian Zachry Faturrahman <alvianzf@gmail.com>
2023-04-18 16:11:28 +0700

    Update and rename whatTimeIsIt.test.js to circumference.test.js

---
+++ .github/tests/circumference.test.js
@@ -0,0 +1,16 @@
+const calculate = require('./../../script');
+
+describe('calculate', () => {
+  test('calculates the circumference of a triangle given its height and weight', () => {
+    // Define the inputs to the function
+    const height = 10;
+    const weight = 20;
+
+    // Define the expected result
+    const expectedResult = 100;
+
+    // Call the function and check the result
+    const result = calculate(height, weight);
+    expect(result).toBe(expectedResult);
+  });
+});
